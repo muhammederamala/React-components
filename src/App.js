7db29fd3d5ac4f588707acfaaf858a69fdb2a94a
muhammederamala <108819976+muhammederamala@users.noreply.github.com>
2023-05-14 22:59:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/login/login'
+import SignupPage from './pages/signup/signup'
+function App() {
+  return (
+    <div>
+      
+      <SignupPage />
+
+    </div>
+  );
+}
+
+export default App;
